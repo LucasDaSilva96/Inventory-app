d95be1e97deb925c8a2932a6e233b6801311820a
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-05-23 21:51:05 +0200

    Started building the edit category page/functionality && added a fallback in case the selected category is not found

--- client/src/pages/EditCategory.jsx
+++ client/src/pages/EditCategory.jsx
@@ -28,59 +28,73 @@ function EditCategory() {
 
   return (
     <section className="flex w-screen h-screen items-center justify-center">
-      <div
-        style={{
-          width: "50%",
-          minWidth: "375px",
-          maxWidth: "700px",
-        }}
-      >
-        <Card className="py-4">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <p className="text-tiny uppercase font-bold">Number of items</p>
-            <small className="text-default-500">{CATEGORY.items.length}</small>
-            <h4 className="font-bold text-large">{CATEGORY.title}</h4>
-          </CardHeader>
-          <CardBody className="overflow-visible py-2">
-            <Image
-              style={{ borderRadius: "10px" }}
-              loading="lazy"
-              isBlurred
-              alt={CATEGORY.title}
-              className="object-cover rounded-xl"
-              src={file}
-              width={280}
-            />
-            <input
-              type="file"
-              onChange={handleImageChange}
-              style={{ padding: "10px 0", maxWidth: "200px", overflow: "auto" }}
-            />
-            <div
-              style={{
-                marginTop: "40px",
-                display: "flex",
-                flexDirection: "column",
-                gap: "10px",
-              }}
-            >
-              <Input
-                type="text"
-                label="Title"
-                value={CATEGORY.title}
-                onInput={(e) =>
-                  SETCATEGORY({ ...CATEGORY, title: e.target.value })
-                }
+      {CATEGORY ? (
+        <div
+          style={{
+            width: "50%",
+            minWidth: "375px",
+            maxWidth: "700px",
+          }}
+        >
+          <Card className="py-4">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+              <p className="text-tiny uppercase font-bold">Number of items</p>
+              <small className="text-default-500">
+                {CATEGORY.items.length}
+              </small>
+              <h4 className="font-bold text-large">{CATEGORY.title}</h4>
+            </CardHeader>
+            <CardBody className="overflow-visible py-2">
+              <Image
+                style={{ borderRadius: "10px" }}
+                loading="lazy"
+                isBlurred
+                alt={CATEGORY.title}
+                className="object-cover rounded-xl"
+                src={file}
+                width={280}
               />
-              <Button color="success">Save</Button>
-              <Button color="danger" onPress={() => navigate("/inventory")}>
-                Cancel
-              </Button>
-            </div>
-          </CardBody>
-        </Card>
-        <div></div>
-      </div>
+              <input
+                type="file"
+                onChange={handleImageChange}
+                style={{
+                  padding: "10px 0",
+                  maxWidth: "200px",
+                  overflow: "auto",
+                }}
+              />
+              <div
+                style={{
+                  marginTop: "40px",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "10px",
+                }}
+              >
+                <Input
+                  type="text"
+                  label="Title"
+                  value={CATEGORY.title}
+                  onInput={(e) =>
+                    SETCATEGORY({ ...CATEGORY, title: e.target.value })
+                  }
+                />
+                <Button color="success">Save</Button>
+                <Button color="danger" onPress={() => navigate("/inventory")}>
+                  Cancel
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+        </div>
+      ) : (
+        <div className="w-full h-full flex items-center justify-center gap-3">
+          <h1>The selected category was not found. Try again</h1>
+          <Button color="primary" onPress={() => navigate("/inventory")}>
+            Go back
+          </Button>
+        </div>
+      )}
     </section>
   );
 }
